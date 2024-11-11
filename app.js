0a3df618553104c2501e4499d76ac7100cb76990
Danish Yousuf Mir <danishmir9991@gmail.com>
2024-11-11 23:44:55 +0530

    updated

---
+++ app.js
@@ -0,0 +1,16 @@
+import express from "express";
+
+const app = express();
+
+const port = process.env.PORT || 8083
+
+app.get("/", (req, res) => {
+    res.status(200).json({
+        success : true,
+        data : "hello world"
+    })
+})
+
+app.listen(port, () => {
+    console.log(`server is running on port ${port}`)
+});
